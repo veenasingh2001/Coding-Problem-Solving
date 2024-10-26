5b306894cec8d471a04b5682f50f62cb5f63a68f
unknown <singhveena2001@gmail.com>
2024-10-25 17:50:04 -0700

    sort an array in Java

---
+++ ShortArry.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class ShortArry {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, -1, -2, 4};
+
+        // Sort the array
+        Arrays.sort(array);
+
+        // Print the sorted array
+        System.out.println(Arrays.toString(array));
+    }
+}
